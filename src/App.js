68a5c17e7c4f236451b09ad458ff0372f383e93a
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-07 19:19:30 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MenuHome from "./Page/MenuHome";
+
+function App() {
+  return (
+    <div>
+      <MenuHome />
+      <br />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/appRoute.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import MenuMakanan from "./Page/MenuMakanan";
+import MenuMinuman from "./Page/MenuMinuman";
+import Kontak from "./Page/Kontak";
+
+const AppRoute = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} exact />
+      <Route path="/menu_makanan" element={<MenuMakanan />} />
+      <Route path="/menu_minuman" element={<MenuMinuman />} />
+      <Route path="/kontak" element={<Kontak />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRoute;
